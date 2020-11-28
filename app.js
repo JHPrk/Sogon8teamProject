2fc7e934d09a8a7cc5c4ca64c7f8d03ac4185176
Jack Park <qkrwogus1236@gmail.com>
2020-11-28 13:48:52 +0900

    check 2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var socketio = require('./routes/socketio');
+var ejs = require('ejs');
 
 
 var homepage = require('./routes/homepage')
@@ -22,7 +23,7 @@ var app = express();
 app.set('port',3000);// //52270
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -33,6 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// URL 추가
 app.use('/', homepage);
 app.use('/classroom', classroom)
 app.use('/assignment', assignment_room)
--- routes/homepage.js
+++ routes/homepage.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path')
-
+var ejs = require('ejs');
+var fs = require('fs')
+	
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
-	res.sendFile(path.resolve(__dirname + '/../views/index.html'));
+	var data = ''
+	var loginInfo = ejs.render(fs.readFileSync(path.resolve(__dirname + '/../views/login_box.ejs'), 'utf8'), {})
+	if(req.cookies.name)
+	{
+		data = req.cookies.name
+		loginInfo = ejs.render(fs.readFileSync(path.resolve(__dirname + '/../views/login_display.ejs'), 'utf8'), {name: data})
+	}
+	res.render('index',{loginBox : loginInfo});
 });
 
 module.exports = router;
